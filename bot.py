e1ffd1a2ecec9c766ae4d6d549714575b99eddef
K.S <del12dmc@gmail.com>
2022-10-23 20:35:03 +0300

    Minor changes in Start command DB CRUD operations attempts

--- bot.py
+++ bot.py
@@ -1,12 +1,38 @@
 from telebot import TeleBot
 from decouple import config
 
+from mongo import db, users
+
 
 bot = TeleBot(config("TELEGRAM_TOKEN"))
 
 
 @bot.message_handler(commands=["start"])
 def start_handler(message):
+    if not db.users.find(
+            {
+                "_id": message.chat.id
+            }
+    ):
+        users.insert_one(
+            {
+                "_id": message.chat.id,
+                "name": message.chat.username,
+                "friends": {},
+            }
+        )
+    print(dir(db.users.database))
+    # print(list(db.users.find({})))
+    db.users.update_one(
+        {
+            "_id": message.chat.id
+        },
+        {
+            "$set": {
+                "friends": "No friends"
+            }
+        }
+    )
 
     bot.send_message(
         chat_id=message.chat.id,
@@ -16,8 +42,10 @@ def start_handler(message):
              f"Username:{message.chat.username},\n"
              f"First_name:{message.chat.first_name},\n"
              f"Last_name:{message.chat.last_name}, \n"
-             f"Photo:{message.chat.photo}",
+             f"Photo:{message.chat.photo},\n"
+             f"Friends:{list(db.users.find({'_id':message.chat.id}))[0]['friends']}",
     )
+    print(list(db.users.find({})))
 
 
 bot.infinity_polling()
--- mongo.py
+++ mongo.py
@@ -5,20 +5,24 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 client = MongoClient(
-    f"mongodb+srv://{config('MONGO_LOGIN')}:{config('MONGO_PASSWORD')}@cluster0.tmykjbu.mongodb.net/?retryWrites=true&w=majority",
+    f"mongodb+srv://{config('MONGO_LOGIN')}:{config('MONGO_PASSWORD')}"
+    f"@cluster0.tmykjbu.mongodb.net/?retryWrites=true&w=majority",
     server_api=ServerApi('1'))
-db = client.test
 
+db = client.bday_db
 collection = db.test_collection
+users = db.users
 
-post = {"author": "Del",
-        "text": "My birth day",
-        "tags": ["mongodb", "python", "pymongo"],
-        "date": datetime.utcnow()}
+
+# post = {"author": "Del",
+#         "text": "My birth day",
+#         "tags": ["mongodb", "python", "pymongo"],
+#         "date": datetime.utcnow()}
 
 
 if __name__ == '__main__':
-    posts = db.posts
-    post_id = posts.insert_one(post).inserted_id
-    print(post_id)
-    print(db.list_collection_names())
+    pass
+    # posts = db.posts
+    # post_id = posts.insert_one(post).inserted_id
+    # print(post_id)
+    # print(db.list_collection_names())
